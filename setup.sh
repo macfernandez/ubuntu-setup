46623cad2bd939d200d3556e3ca9cf4d65fee46c
Macarena Fernández Urquiza <m.fernandezurquiza@gmail.com>
2024-06-02 19:29:27 -0300

    add vscode extensions

--- setup.sh
+++ setup.sh
@@ -19,8 +19,35 @@ wget -O "${TMP_FOLDER}/google-chrome-stable_current_amd64.deb" \
     https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i "${TMP_FOLDER}/google-chrome-stable_current_amd64.deb"
 
-# TODO: add extensions
 sudo snap install --classic code
+code --install-extension \
+    waderyan.gitblame \
+    donjayamanne.githistory \
+    mhutchie.git-graph \
+    eamodio.gitlens \
+    ms-toolsai.jupyter \
+    ms-toolsai.vscode-jupyter-cell-tags \
+    ms-toolsai.jupyter-keymap \
+    ms-toolsai.jupyter-renderers \
+    ms-toolsai.vscode-jupyter-slideshow \
+    mathematic.vscode-latex \
+    torn4dom4n.latex-support \
+    tecosaur.latex-utilities \
+    james-yu.latex-workshop \
+    nickfode.latex-formatter \
+    berublan.vscode-log-viewer \
+    ms-python.vscode-pylance \
+    ms-python.python \
+    ms-python.debugpy \
+    mechatroner.rainbow-csv \
+    redhat.vscode-yaml \
+    ms-azuretools.vscode-docker \
+    ms-vscode-remote.remote-containers \
+    rangav.vscode-thunder-client \
+    ms-vscode-remote.remote-containers \
+    davidanson.vscode-markdownlint
+
+
 sudo snap install spotify
 
 sudo apt-get install \
